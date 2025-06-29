42e996ba66de06c5065d159a4ada6cc63e88569e
Christopher Martínez <chrismartinez.2054@gmail.com>
2025-06-28 19:34:32 -0600

    Initial Commit

---
+++ PasswordManager/PasswordManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerApp.swift
+//  PasswordManager
+//
+//  Created by Christopher Martínez on 28/6/25.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
